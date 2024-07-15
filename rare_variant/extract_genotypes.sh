864b3bf3841c849504fceecad5c89e365eb4a463
mvaudel <mvaudel@gmail.com>
2024-07-15 11:43:32 +0200

    Extract rare variant

--- rare_variant/extract_genotypes.sh
+++ rare_variant/extract_genotypes.sh
@@ -4,6 +4,8 @@
 ##
 
 # Make pgen files with the ids and variants of interest
-plink2 --bfile /mnt/archive/moba/geno/MOBAGENETICS_1.0/genotypes-base/imputed/all/plink/19 --extract rare_variant --make-pgen --out /mnt/work/marc/hg/variants/rare_variant
-plink2 --pfile /mnt/work/marc/hg/variants/rare_variant --export-allele rare_variant --export A --out /mnt/work/marc/hg/variants/rare_variant
+plink2 --bfile /mnt/archive/moba/geno/MOBAGENETICS_1.0/genotypes-base/imputed/all/plink/19 --extract rare_variant_19 --make-pgen --out /mnt/work/marc/hg/variants/rare_variant_19
+plink2 --pfile /mnt/work/marc/hg/variants/rare_variant_19 --export-allele rare_variant_19 --export A --out /mnt/work/marc/hg/variants/rare_variant_19
+plink2 --bfile /mnt/archive/moba/geno/MOBAGENETICS_1.0/genotypes-base/imputed/all/plink/10 --extract rare_variant_10 --make-pgen --out /mnt/work/marc/hg/variants/rare_variant_10
+plink2 --pfile /mnt/work/marc/hg/variants/rare_variant_10 --export-allele rare_variant_10 --export A --out /mnt/work/marc/hg/variants/rare_variant_10
 
